44ac69310b5c1bf276c1a8c1f3ee24a1b6abddd8
airdipu <airdipu@gmail.com>
2023-03-04 20:30:37 +0200

    added modified status

--- app.py
+++ app.py
@@ -1,37 +1,46 @@
-from flask import Flask, render_template, request, send_file, jsonify
-from pytube import YouTube
-import os
+from flask import Flask, request, render_template, jsonify, redirect, url_for
+from youtube_dl import YoutubeDL
 
 app = Flask(__name__)
-app.config['TIMEOUT'] = 60
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        url = request.form['url']
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': 'audio.%(ext)s',
+            'noplaylist': True,
+            'progress_hooks': [progress_hook],
+        }
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        return redirect(url_for('status'))
     return render_template('index.html')
-
-@app.route('/', methods=['POST'])
-def download():
-    url = request.form['url']
-    yt = YouTube(url, on_progress_callback=progress_function)
-    audio_stream = yt.streams.filter(only_audio=True).first()
-    output_file_path = "my_song.mp3"
-    audio_stream.download(output_path=".", filename=output_file_path)
-    return send_file(output_file_path, as_attachment=True)
 
 @app.route('/progress')
 def progress():
-    if 'progress' in globals():
-        return jsonify(progress=progress)
-    else:
-        return jsonify(progress=0)
+    return jsonify({'progress': progress_dict})
 
-def progress_function(stream, chunk, bytes_remaining):
-    total_size = stream.filesize
-    bytes_downloaded = total_size - bytes_remaining
-    percentage = bytes_downloaded / total_size * 100
-    kb_downloaded = bytes_downloaded / 1024
-    global progress
-    progress = {'percentage': percentage, 'kb_downloaded': kb_downloaded}
+@app.route('/status')
+def status():
+    return render_template('status.html', kb_downloaded=progress_dict['kb_downloaded'], percentage=progress_dict['percentage'])
+
+@app.route('/back')
+def back():
+    return redirect(url_for('index'))
+
+def progress_hook(d):
+    if d['status'] == 'finished':
+        progress_dict['kb_downloaded'] = d.get('total_bytes', 0) / 1024
+        progress_dict['percentage'] = 100.0
+    elif d['status'] == 'downloading':
+        if d.get('total_bytes'):
+            kb_downloaded = d.get('downloaded_bytes', 0) / 1024
+            total_size = d.get('total_bytes', 0) / 1024
+            progress_dict['kb_downloaded'] = kb_downloaded
+            progress_dict['percentage'] = (kb_downloaded / total_size) * 100
 
 if __name__ == '__main__':
+    progress_dict = {'kb_downloaded': 0, 'percentage': 0}
     app.run(debug=True)
