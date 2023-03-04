01415d342abee5e382018d3a11ab8aebc5a752d9
airdipu <airdipu@gmail.com>
2023-03-04 19:30:46 +0200

    updated for download progress

--- app.py
+++ app.py
@@ -23,6 +23,26 @@ def progress_function(stream, chunk, bytes_remaining):
     percentage = bytes_downloaded / total_size * 100
     kb_downloaded = bytes_downloaded / 1024
     print(f"Downloaded {kb_downloaded:.2f} KB ({percentage:.2f}%) of {total_size / 1024 / 1024:.2f} MB")
+    # send progress information to client using WebSocket
+    socketio.emit('progress', {'percentage': percentage, 'kb_downloaded': kb_downloaded})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # set up Flask-SocketIO
+    from flask_socketio import SocketIO, emit
+    app.config['SECRET_KEY'] = 'secret!'
+    socketio = SocketIO(app)
+
+    @app.route('/progress')
+    def progress():
+        return render_template('progress.html')
+
+    # handle WebSocket connections from client
+    @socketio.on('connect')
+    def test_connect():
+        print('Client connected')
+
+    @socketio.on('disconnect')
+    def test_disconnect():
+        print('Client disconnected')
+
+    socketio.run(app, debug=True)
