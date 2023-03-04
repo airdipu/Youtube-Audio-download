b1d10660135b22962dea68f75d91fb2826a7edb2
airdipu <airdipu@gmail.com>
2023-03-04 19:38:28 +0200

    updated progress bar

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, jsonify
 from pytube import YouTube
 import os
 
@@ -17,32 +17,20 @@ def download():
     audio_stream.download(output_path=".", filename=output_file_path)
     return send_file(output_file_path, as_attachment=True)
 
+@app.route('/progress')
+def progress():
+    if 'progress' in globals():
+        return jsonify(progress=progress)
+    else:
+        return jsonify(progress=0)
+
 def progress_function(stream, chunk, bytes_remaining):
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
     percentage = bytes_downloaded / total_size * 100
     kb_downloaded = bytes_downloaded / 1024
-    print(f"Downloaded {kb_downloaded:.2f} KB ({percentage:.2f}%) of {total_size / 1024 / 1024:.2f} MB")
-    # send progress information to client using WebSocket
-    socketio.emit('progress', {'percentage': percentage, 'kb_downloaded': kb_downloaded})
+    global progress
+    progress = {'percentage': percentage, 'kb_downloaded': kb_downloaded}
 
 if __name__ == '__main__':
-    # set up Flask-SocketIO
-    from flask_socketio import SocketIO, emit
-    app.config['SECRET_KEY'] = 'secret!'
-    socketio = SocketIO(app)
-
-    @app.route('/progress')
-    def progress():
-        return render_template('progress.html')
-
-    # handle WebSocket connections from client
-    @socketio.on('connect')
-    def test_connect():
-        print('Client connected')
-
-    @socketio.on('disconnect')
-    def test_disconnect():
-        print('Client disconnected')
-
-    socketio.run(app, debug=True)
+    app.run(debug=True)
